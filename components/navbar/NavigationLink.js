707e9840e6b0a0284eb5730b9f0f5b0e73476341
alain-kubwayo <ahkubwayo@gmail.com>
2022-11-01 19:03:44 +0200

    --amend

--- components/navbar/NavigationLink.js
+++ components/navbar/NavigationLink.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const NavigationLink = ({ text }) => {
   return (
-    <Link href="#">
+    <Link href="">
       <li className="px-4 py-3 text-base text-white font-bolded">{text}</li>
     </Link>
   );
